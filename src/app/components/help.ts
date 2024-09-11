1e4b69cd7e7ecc3e5f534db0d2b808fb5215aec5
AkimZmerli <akimdotzmerli@outlook.com>
2024-09-11 17:30:19 +0200

    commit of the day

--- src/app/components/help.ts
+++ src/app/components/help.ts
@@ -1,3 +1,3 @@
 export function help(): string {
-  return "Available commands: help, about, weather, contact, clear, banner, secret, delete";
+  return "Available commands: help, about, weather, minesweeper, contact, clear, banner, secret, delete";
 }
