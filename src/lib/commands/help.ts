7379d6999ee46027e25683166f32fc2fa8f81b27
AkimZmerli <akimdotzmerli@outlook.com>
2024-09-21 11:59:15 +0200

    refactoring help response

--- src/lib/commands/help.ts
+++ src/lib/commands/help.ts
@@ -1,7 +1,7 @@
 export function help(): number {
   const myNumber = 123456789;
   const convertedNumber = myNumber.toString();
-  const result = convertedNumber.split("").reverse().join("").map(()=>);
+  const result = convertedNumber.split("").reverse().join("");
   return `Your number is ${result}`;
 }
 
