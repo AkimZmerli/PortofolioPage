4dae58c4c3ff9a91d6441c5f5a899d0f85240955
AkimZmerli <akimdotzmerli@outlook.com>
2024-09-22 14:15:33 +0200

    refactoring help response

--- src/lib/commands/help.ts
+++ src/lib/commands/help.ts
@@ -2,7 +2,7 @@ export function help(): number {
   const myNumber = 123456789;
   const convertedNumber = myNumber.toString();
   const result = convertedNumber.split("").reverse().join("");
-  return `Your number is ${result}`;
+  return `Your numbers are ${result}`;
 }
 
 // "Available commands: help, about, weather, minesweeper, contact, clear, banner, secret, delete";
