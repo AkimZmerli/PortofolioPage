27ee8077c8cba632fd18a02ec0a76e592728117e
AkimZmerli <akimdotzmerli@outlook.com>
2024-09-24 11:10:30 +0200

    restore help function

--- src/lib/commands/help.ts
+++ src/lib/commands/help.ts
@@ -1,8 +1,9 @@
-export function help(): number {
-  const myNumber = 123456789;
-  const convertedNumber = myNumber.toString();
-  const result = convertedNumber.split("").reverse().join("");
-  return `Your number is ${result}`;
+export function help(): string {
+  return "Available commands: help, about, weather, minesweeper, contact, clear, banner, secret, delete";
 }
 
-// "Available commands: help, about, weather, minesweeper, contact, clear, banner, secret, delete";
+// : number {
+//   const myNumber = 123456789;
+//   const convertedNumber = myNumber.toString();
+//   const result = convertedNumber.split("").reverse().join("");
+//   return `Your number is ${result}`
